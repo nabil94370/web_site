cb9412abe449832951443aded04d676b59d3b440
nabil94370 <nabilabbes94370@gmail.com>
2023-04-11 02:19:40 +0200

    vérification nom d'utilisateur

--- admin/inscription.php
+++ admin/inscription.php
@@ -1,17 +1,27 @@
 <?php
 require_once ('db_conn.php');
 $message = '';
+$erreur = '';
 
 if (!empty($_POST)) {
-$nom_utilisateur = $_POST['uname'];
-$Mot_de_passe = $_POST['password'];
+    $nom_utilisateur = $_POST['uname'];
+    $Mot_de_passe = $_POST['password'];
 
-$CreateSql = "INSERT INTO `users` (user_name,passwords) VALUES ('$nom_utilisateur', '$Mot_de_passe')";
-$res = mysqli_query($conn, $CreateSql) or die(mysqli_error($conn));
-    if ($res) {
-        $message = "Création utilisateur $nom_utilisateur OK";
-    } else {
-        $message = "Erreur d'inscription";
+    // Vérifier si l'utilisateur existe déjà
+    $checkSql = "SELECT user_name FROM users WHERE user_name='$nom_utilisateur'";
+    $checkResult = mysqli_query($conn, $checkSql);
+    $count = mysqli_num_rows($checkResult);
+    
+    if ($count == 0) { // Si le nom d'utilisateur n'existe pas, insérer l'utilisateur dans la base de données
+        $CreateSql = "INSERT INTO `users` (user_name,passwords) VALUES ('$nom_utilisateur', '$Mot_de_passe')";
+        $res = mysqli_query($conn, $CreateSql) or die(mysqli_error($conn));
+        if ($res) {
+            $message = "Création utilisateur $nom_utilisateur OK";
+        } else {
+            $erreur = "Erreur d'inscription";
+        }
+    } else { // Si le nom d'utilisateur existe déjà, afficher un message d'erreur
+        $erreur = "Le nom d'utilisateur $nom_utilisateur existe déjà";
     }
 }
 ?>
@@ -19,19 +29,19 @@ $res = mysqli_query($conn, $CreateSql) or die(mysqli_error($conn));
 <!DOCTYPE html>
 <html>
 <head>
-	<title>INSCRIPTION </title>
-	<link rel="stylesheet" type="text/css" href="css.css">
+    <title>INSCRIPTION </title>
+    <link rel="stylesheet" type="text/css" href="css.css">
 </head>
 <body>
      <form action="" method="post" class="all_form">
-     	<h2>INSCRIPTION</h2>
-     	<label>Nom d'utilisateur</label>
-     	<input type="text" name="uname" placeholder="Nom d'utilisateur"><br>
+        <h2>INSCRIPTION</h2>
+        <label>Nom d'utilisateur</label>
+        <input type="text" name="uname" placeholder="Nom d'utilisateur"><br>
 
-     	<label>Mot de passe</label>
-     	<input type="password" name="password" placeholder="Mot De Passe"><br>
+        <label>Mot de passe</label>
+        <input type="password" name="password" placeholder="Mot De Passe"><br>
 
-     	<input type="submit" name="submit" value="Inscription">
+        <input type="submit" name="submit" value="Inscription">
          <a href="index.php">
             <button class="btn btn-success m-3" type="button">
                 login
@@ -40,14 +50,13 @@ $res = mysqli_query($conn, $CreateSql) or die(mysqli_error($conn));
         <?php if (isset($message)) { ?>
             <div class="alert alert-success" role="alert">
                 <?php echo $message; ?>
-            </div><?php } ?>
-            <?php if (isset($erreur)) { ?>
+            </div>
+        <?php } ?>
+        <?php if (isset($erreur)) { ?>
             <div class="alert alert-danger" role="alert">
                 <?php echo $erreur; ?>
-            </div><?php } ?>
+            </div>
+        <?php } ?>
      </form>
-     
-
-
 </body>
 </html>
